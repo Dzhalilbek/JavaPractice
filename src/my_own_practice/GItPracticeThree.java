af4a2667ca2b9ba58082f5e28d0b1281f6ce9fbe
Dzhalilbek <126055941+Dzhalilbek@users.noreply.github.com>
2023-03-13 21:12:02 -0700

    Update GItPracticeThree.java
    These changes are from upstream git repo

--- src/my_own_practice/GItPracticeThree.java
+++ src/my_own_practice/GItPracticeThree.java
@@ -4,7 +4,12 @@ public class GItPracticeThree {
 
 	public int div(int a, int b) {
 		int res = a / b;
-		return res;
-		
+		return res;		
+	}
+	
+	public int test(int x, int y, String message){
+		System.out.println(massage);
+		int z = x+y;
+		return z;
 	}
 }
